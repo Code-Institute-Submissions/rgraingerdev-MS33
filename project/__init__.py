358e92ff70eb50df6e2008cc7ab8aaad75914ca4
rgraingerdev <ryan.grainger2115@gmail.com>
2023-07-19 22:11:12 +0100

    Page now runs in browser all pages work

--- project/__init__.py
+++ project/__init__.py
@@ -4,10 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env # noqa
 
+
+
 app = Flask(__name__)
 app.config["secret_key"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+
 db = SQLAlchemy(app)
 
-from project import routes #noqa
+from project import routes  #noqa
--- project/routes.py
+++ project/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template
+from flask import render_template, url_for
 from project import app, db
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html", home=home)
 
+@app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", contact=contact)
 
+@app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", about=about)
