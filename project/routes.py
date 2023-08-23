e2b8cf65eab4ec033dc87571106e37af73bf9b7c
rgraingerdev <ryan.grainger2115@gmail.com>
2023-08-23 21:51:20 +0100

    Allow data to push to DB Encrypt PW added

--- project/routes.py
+++ project/routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from project import app, db
+from sqlalchemy import text
+from project.models import users
 
 @app.route("/")
 def home():
@@ -15,6 +17,27 @@ def idea3():
 
 @app.route("/signin", methods=["GET", "POST"])
 def signin():
-    return render_template("signin.html", signin=signin)
+    if request.method == "POST":
+        fname = request.form.get("fname")
+        sname = request.form.get("sname")
+        password_hash = request.form.get("password")
+        email = request.form.get("email")
+
+        new_user = users(fname = fname, sname = sname, email = email)
+        new_user.set_password(password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("idea3"))
+    return render_template("signin.html", title="Sign Up", signin=signin)
+
+
+@app.route('/test-connection')
+def test_connection():
+    try:
+        count = text('SELECT COUNT(*) FROM users')
+        return f"Connection successful! Total records: {count}"
+    except Exception as e:
+        return f"Connection failed: {str(e)}"
 
 
