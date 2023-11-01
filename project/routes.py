233c175ea64054ca31e17ef1321f22bad638d6c8
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-11-01 09:31:30 +0000

    Added all PEP8 corrections

--- project/routes.py
+++ project/routes.py
@@ -1,28 +1,38 @@
+"""Defines routes and uses of pages"""
 from flask import Flask, render_template, url_for, request, redirect, flash
-from flask_login import login_user, logout_user, LoginManager, current_user, UserMixin, login_required
+from flask_login import (login_user, logout_user,
+                         LoginManager,current_user,UserMixin,login_required)
 from flask_sqlalchemy import SQLAlchemy
-from project import app, db
 from sqlalchemy import text
-from project.models import users, reviews, ContactMessage, lessons
 import bcrypt
 from bcrypt import hashpw, gensalt, checkpw
+from project.models import users, reviews, ContactMessage, lessons
+from project import app, db
+
+
+
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'signin'
 
+
 @app.route("/")
 def home():
+    """renders homepage"""
     return render_template("home.html", home=home)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
+    """renders contact form"""
     if request.method == 'POST':
         fname = request.form.get("fname")
         sname = request.form.get("sname")
         email = request.form.get("email")
         subject = request.form.get("subject")
         message = request.form.get("message")
-        new_message = ContactMessage(fname=fname, sname=sname, email=email, subject=subject, message=message)
+        new_message = ContactMessage(
+            fname=fname, sname=sname, email=email, subject=subject, message=message
+            )
         db.session.add(new_message)
         db.session.commit()
 
@@ -31,15 +41,19 @@ def contact():
 @app.route("/timetable")
 @login_required
 def timetable():
+    """renders Timeline page"""
     return render_template("timetable.html", timetable=timetable)
 
 @app.route("/reviews")
 def display_reviews():
+    """shows review"""
     review = reviews.query.join(users).add_columns(users.fname, reviews.content, reviews.id).all()
     return render_template("reviews.html", review=review)
-    
+
+
 @app.route("/create_message", methods=["GET", "POST"])
 def create_message():
+    """renders create message page"""
     if request.method == 'POST':
 
         content = request.form.get("content")
@@ -50,9 +64,9 @@ def create_message():
         return redirect(url_for("timetable"))
     return render_template("timetable.html", timetable=timetable)
 
-
-@app.route("/edit_review/<int:review_id>", methods=["GET", "POST"] ) 
+@app.route("/edit_review/<int:review_id>", methods=["GET", "POST"])
 def edit_review(review_id):
+    """renders review edit"""
     review = reviews.query.get_or_404(review_id)
     if request.method == 'POST':
         review.content = request.form.get("content")
@@ -62,7 +76,8 @@ def edit_review(review_id):
 
 @app.route("/delete_review/<int:review_id>")
 def delete_review(review_id):
-    review= reviews.query.get_or_404(review_id)
+    """deletes review"""
+    review = reviews.query.get_or_404(review_id)
 
     if review:
         db.session.delete(review)
@@ -71,15 +86,18 @@ def delete_review(review_id):
 
 @app.route("/view_messages")
 def view_messages():
+    """view contacts mate to site owner"""
     messages = ContactMessage.query.all()
     return render_template("view_messages.html", messages=messages)
 
 @login_manager.user_loader
 def load_user(user_id):
+    """loads user details"""
     return users.query.get(int(user_id))
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    """creates user account"""
     if request.method == "POST":
         fname = request.form.get("fname")
         sname = request.form.get("sname")
@@ -96,11 +114,12 @@ def signup():
 
         flash("Registration Successful", 'success')
         return redirect(url_for("signin"))
-    
+
     return render_template('signup.html', title="Sign Up", signup=signup)
 
 @app.route("/signin", methods=["GET", "POST"])
 def signin():
+    """Signs user into site"""
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
@@ -113,12 +132,9 @@ def signin():
             return redirect(url_for("timetable"))
         flash("Login Unsuccessful", 'danger')
     return render_template("signin.html", title="Sign In", signin=signin)
-            
+
 @app.route("/logout")
 def logout():
+    '''Logs user out'''
     logout_user()
     return redirect(url_for("home"))
-
-
-
-
