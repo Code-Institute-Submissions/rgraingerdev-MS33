01812ebffb229b303fa881939a69993cb22a7232
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-11-01 10:11:50 +0000

    Removed unrequired import from routes

--- project/routes.py
+++ project/routes.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 import bcrypt
 from bcrypt import hashpw, gensalt, checkpw
-from project.models import Users, Reviews, ContactMessage, Lessons
+from project.models import Users, Reviews, ContactMessage
 from project import app, db
 
 
