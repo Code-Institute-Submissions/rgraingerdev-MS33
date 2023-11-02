ff33188c4b2df93eb4194b98186e5d3270c732e1
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-11-02 20:10:00 +0000

    Added missing docsting to env.py

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
+'''Enviroment setup'''
 import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "any_secret_key")
-os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEBUG", "False")
